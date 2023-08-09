e3352231039d491dd298e95eeb10f973a55e1225
Christopher-Deo <chris@christopherdeo.com>
2023-08-09 18:35:47 -0500

    all req's met

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styling.css";
+
+
+export default function Banner() {
+    return (
+        <section>
+            <header>
+                <h1>Orbit Report</h1>
+            </header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </section >
+    );
+}
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,19 @@
-const Buttons = () => {
+import React from 'react';
+import satData from './satData';
+import "./styling.css";
+
+const Buttons = (props) => {
+  const { filterByType, setSat, displaySats } = props;
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className='flex-container'>
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
